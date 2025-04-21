aa07bb1a5ae5a373e4bcff718f73ea7f8df5e3cc
Harsha Vardhan <129319351+harsha654265@users.noreply.github.com>
2025-04-21 10:48:13 +0530

    Update movies.js

--- movies.js
+++ movies.js
@@ -1,94 +1,5 @@
 const movies = [
   {
-    title: "WWE WrestleMania 41 (2025) Night 1",
-    img: "https://1max.top/wp-content/uploads/2025/04/Watch-WWE-WrestleMania-41-2025-Night-2-PPV-Live-42025-April-20th-2025-Online-Full-Show-Free.jpg",
-    description: "WWE WrestleMania 41 Night 2 (2025) showcases epic battles and historic moments as the biggest names in professional wrestling clash for dominance. Don't miss out on the action from the grandest stage of them all!",
-    downloadLinks: [
-      { quality: "HD WWE WrestleMania 41 Night 2", link: "https://gofile.io/d/MTyuSW" }
-    ]
-  },
-  {
-    title: "Game of Thrones S01 (2011)",
-    img: "https://image.tmdb.org/t/p/original/fw9hNiJO0U5MUg2XGyCOlTS3jdS.jpg",
-    description: "Seven noble families fight for control of the mythical land of Westeros. Friction between the houses leads to full-scale war. All while a very ancient evil awakens in the farthest north...",
-    downloadLinks: [
-      { quality: "4K UHD GOT S01E01 MUL", link: "https://new6.gdflix.dad/file/NseSwvJISK" },
-      { quality: "4K UHD GOT S01E02 MUL", link: "https://new6.gdflix.dad/file/RgNpAjB7q2" },
-      { quality: "4K UHD GOT S01E03 MUL", link: "https://new6.gdflix.dad/file/UX512ZJ1RX" },
-      { quality: "4K UHD GOT S01E04 MUL", link: "https://new6.gdflix.dad/file/pEHWCweR5h" },
-      { quality: "4K UHD GOT S01E05 MUL", link: "https://new6.gdflix.dad/file/Z7mDIPdaf2" },
-      { quality: "4K UHD GOT S01E06 MUL", link: "https://new6.gdflix.dad/file/u3b7i9NchN" },
-      { quality: "4K UHD GOT S01E07 MUL", link: "https://new6.gdflix.dad/file/3hAROJM077" },
-      { quality: "4K UHD GOT S01E08 MUL", link: "https://new6.gdflix.dad/file/11Rmt2jijl" },
-      { quality: "4K UHD GOT S01E09 MUL", link: "https://new6.gdflix.dad/file/4A5f89bW5i" },
-      { quality: "4K UHD GOT S01E10 MUL", link: "https://new6.gdflix.dad/file/7vNcjzjmCc" }
-    ]
-  },
-   {
-    title: "Game of Thrones S01 (2011)",
-    img: "https://image.tmdb.org/t/p/original/fw9hNiJO0U5MUg2XGyCOlTS3jdS.jpg",
-    description: "Seven noble families fight for control of the mythical land of Westeros. Friction between the houses leads to full-scale war. All while a very ancient evil awakens in the farthest north...",
-    downloadLinks: [
-      { quality: "4K UHD GOT S01E01 MUL", link: "https://new6.gdflix.dad/file/NseSwvJISK" },
-      { quality: "4K UHD GOT S01E02 MUL", link: "https://new6.gdflix.dad/file/RgNpAjB7q2" },
-      { quality: "4K UHD GOT S01E03 MUL", link: "https://new6.gdflix.dad/file/UX512ZJ1RX" },
-      { quality: "4K UHD GOT S01E04 MUL", link: "https://new6.gdflix.dad/file/pEHWCweR5h" },
-      { quality: "4K UHD GOT S01E05 MUL", link: "https://new6.gdflix.dad/file/Z7mDIPdaf2" },
-      { quality: "4K UHD GOT S01E06 MUL", link: "https://new6.gdflix.dad/file/u3b7i9NchN" },
-      { quality: "4K UHD GOT S01E07 MUL", link: "https://new6.gdflix.dad/file/3hAROJM077" },
-      { quality: "4K UHD GOT S01E08 MUL", link: "https://new6.gdflix.dad/file/11Rmt2jijl" },
-      { quality: "4K UHD GOT S01E09 MUL", link: "https://new6.gdflix.dad/file/4A5f89bW5i" },
-      { quality: "4K UHD GOT S01E10 MUL", link: "https://new6.gdflix.dad/file/7vNcjzjmCc" }
-    ]
-  }, {
-    title: "Game of Thrones S01 (2011)",
-    img: "https://image.tmdb.org/t/p/original/fw9hNiJO0U5MUg2XGyCOlTS3jdS.jpg",
-    description: "Seven noble families fight for control of the mythical land of Westeros. Friction between the houses leads to full-scale war. All while a very ancient evil awakens in the farthest north...",
-    downloadLinks: [
-      { quality: "4K UHD GOT S01E01 MUL", link: "https://new6.gdflix.dad/file/NseSwvJISK" },
-      { quality: "4K UHD GOT S01E02 MUL", link: "https://new6.gdflix.dad/file/RgNpAjB7q2" },
-      { quality: "4K UHD GOT S01E03 MUL", link: "https://new6.gdflix.dad/file/UX512ZJ1RX" },
-      { quality: "4K UHD GOT S01E04 MUL", link: "https://new6.gdflix.dad/file/pEHWCweR5h" },
-      { quality: "4K UHD GOT S01E05 MUL", link: "https://new6.gdflix.dad/file/Z7mDIPdaf2" },
-      { quality: "4K UHD GOT S01E06 MUL", link: "https://new6.gdflix.dad/file/u3b7i9NchN" },
-      { quality: "4K UHD GOT S01E07 MUL", link: "https://new6.gdflix.dad/file/3hAROJM077" },
-      { quality: "4K UHD GOT S01E08 MUL", link: "https://new6.gdflix.dad/file/11Rmt2jijl" },
-      { quality: "4K UHD GOT S01E09 MUL", link: "https://new6.gdflix.dad/file/4A5f89bW5i" },
-      { quality: "4K UHD GOT S01E10 MUL", link: "https://new6.gdflix.dad/file/7vNcjzjmCc" }
-    ]
-  }, {
-    title: "Game of Thrones S01 (2011)",
-    img: "https://image.tmdb.org/t/p/original/fw9hNiJO0U5MUg2XGyCOlTS3jdS.jpg",
-    description: "Seven noble families fight for control of the mythical land of Westeros. Friction between the houses leads to full-scale war. All while a very ancient evil awakens in the farthest north...",
-    downloadLinks: [
-      { quality: "4K UHD GOT S01E01 MUL", link: "https://new6.gdflix.dad/file/NseSwvJISK" },
-      { quality: "4K UHD GOT S01E02 MUL", link: "https://new6.gdflix.dad/file/RgNpAjB7q2" },
-      { quality: "4K UHD GOT S01E03 MUL", link: "https://new6.gdflix.dad/file/UX512ZJ1RX" },
-      { quality: "4K UHD GOT S01E04 MUL", link: "https://new6.gdflix.dad/file/pEHWCweR5h" },
-      { quality: "4K UHD GOT S01E05 MUL", link: "https://new6.gdflix.dad/file/Z7mDIPdaf2" },
-      { quality: "4K UHD GOT S01E06 MUL", link: "https://new6.gdflix.dad/file/u3b7i9NchN" },
-      { quality: "4K UHD GOT S01E07 MUL", link: "https://new6.gdflix.dad/file/3hAROJM077" },
-      { quality: "4K UHD GOT S01E08 MUL", link: "https://new6.gdflix.dad/file/11Rmt2jijl" },
-      { quality: "4K UHD GOT S01E09 MUL", link: "https://new6.gdflix.dad/file/4A5f89bW5i" },
-      { quality: "4K UHD GOT S01E10 MUL", link: "https://new6.gdflix.dad/file/7vNcjzjmCc" }
-    ]
-  }, {
-    title: "Game of Thrones S01 (2011)",
-    img: "https://image.tmdb.org/t/p/original/fw9hNiJO0U5MUg2XGyCOlTS3jdS.jpg",
-    description: "Seven noble families fight for control of the mythical land of Westeros. Friction between the houses leads to full-scale war. All while a very ancient evil awakens in the farthest north...",
-    downloadLinks: [
-      { quality: "4K UHD GOT S01E01 MUL", link: "https://new6.gdflix.dad/file/NseSwvJISK" },
-      { quality: "4K UHD GOT S01E02 MUL", link: "https://new6.gdflix.dad/file/RgNpAjB7q2" },
-      { quality: "4K UHD GOT S01E03 MUL", link: "https://new6.gdflix.dad/file/UX512ZJ1RX" },
-      { quality: "4K UHD GOT S01E04 MUL", link: "https://new6.gdflix.dad/file/pEHWCweR5h" },
-      { quality: "4K UHD GOT S01E05 MUL", link: "https://new6.gdflix.dad/file/Z7mDIPdaf2" },
-      { quality: "4K UHD GOT S01E06 MUL", link: "https://new6.gdflix.dad/file/u3b7i9NchN" },
-      { quality: "4K UHD GOT S01E07 MUL", link: "https://new6.gdflix.dad/file/3hAROJM077" },
-      { quality: "4K UHD GOT S01E08 MUL", link: "https://new6.gdflix.dad/file/11Rmt2jijl" },
-      { quality: "4K UHD GOT S01E09 MUL", link: "https://new6.gdflix.dad/file/4A5f89bW5i" },
-      { quality: "4K UHD GOT S01E10 MUL", link: "https://new6.gdflix.dad/file/7vNcjzjmCc" }
-    ]
-  }, {
     title: "Game of Thrones S01 (2011)",
     img: "https://image.tmdb.org/t/p/original/fw9hNiJO0U5MUg2XGyCOlTS3jdS.jpg",
     description: "Seven noble families fight for control of the mythical land of Westeros. Friction between the houses leads to full-scale war. All while a very ancient evil awakens in the farthest north...",
@@ -121,13 +32,6 @@ const movies = [
       { quality: "4K UHD GOT S02E09 MUL", link: "https://new6.gdflix.dad/file/QuWYUkSUBh" },
       { quality: "4K UHD GOT S02E10 MUL", link: "https://new6.gdflix.dad/file/QuWYUkSUBh" }
     ]
-  },
-  {
-    title: "WWE WrestleMania 41 (2025) Night 2",
-    img: "https://1max.top/wp-content/uploads/2025/04/Watch-WWE-WrestleMania-41-2025-Night-2-PPV-Live-42025-April-20th-2025-Online-Full-Show-Free.jpg",
-    description: "WWE WrestleMania 41 Night 2 (2025) showcases epic battles and historic moments as the biggest names in professional wrestling clash for dominance. Don't miss out on the action from the grandest stage of them all!",
-    downloadLinks: [
-      { quality: "HD WWE WrestleMania 41 Night 2", link: "https://gofile.io/d/MTyuSW" }
-    ]
   }
+  
 ];
