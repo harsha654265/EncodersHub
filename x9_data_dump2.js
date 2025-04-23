671382a859618edfd87e728f5502e7a23a2eca37
Harsha Vardhan <129319351+harsha654265@users.noreply.github.com>
2025-04-23 23:42:03 +0530

    Update x9_data_dump2.js

--- x9_data_dump2.js
+++ x9_data_dump2.js
@@ -1,1 +1,14 @@
-..
+const moviestext = [
+  {
+    "title": "Rebel (2012)",
+    "img": "https://image.tmdb.org/t/p/original/dNTzorrVmcJ25fUGzs9FfkoXb0P.jpg",
+    "description": "After his parents are brutally murdered, a man goes on a quest to find his parents' killers. He finds that his own uncle is involved in the killings.",
+    "downloadLinks": [
+      { "quality": "1080p  REMuXx  37.6GB", "link": "https://hubcloud.bz/drive/bfi8hsfruhfedna" },
+      { "quality": "1080p BLU-RaY AVC HIN +TEL  14.5GB", "link": "https://new17.gdtot.dad/file/121593038835" },
+      { "quality": "1080p SUNNxT WEB-DL  5.65GB ", "link": "https://scloud.snub-tiptop-half.workers.dev/?download=86af07bc3c22a5fbf4b89e34459cfc52%3Ae1ccb5fac2c64dff0c4c14feb26d644d8301cad8e31f403bde6f9762b875c01c37da91b7619eb12da57d85cffdf5fdab178dbc25536a6c816c7e7321e296c118f2da6eab36de1159a2b08d42933eb1a1e86afaccd47e4bf4d69ca4f379c8640019083093519f61317b858445f5ce640a671835a09b5e8b2b2d09fa60aab14782355bb14afd588c644f127583ca8a242ba7f57bd43d3e013c598eb6fa030f9d10bf05598c5ebda691b0cd9bfb39fee571" },
+      { "quality": "1080p BLU-RaY HEVC  4.7GB", "link": "https://scloud.snub-tiptop-half.workers.dev/?download=3f668a270ab75aa0de78417d7e20f580%3Aef45244b932efdbd0d7a8b8731845b62a638dd6c9779b5d3f5ac2288a09e46c23dbeddedc93d8e8408387d28cd738e50bc828eed445a1697c1531e7198c7eba791b4531df4a049837bf638ad091e055203982f6a9bdad31ff79849e42747d9a188a7b9e5de274d25e402edd3d45f9b988df8da84fcb5c10891aea8133eb35ae88c6489bc147b8ac09e1b86b0e8c2a23ba5161d16ec3efc27c04e62c5f64d4a627af163b6e308167c5045b10e12913c15" }
+      
+    ]
+  },
+];
