7297ec372f2443a5888064250dc2a84a6792227f
Harsha Vardhan <129319351+harsha654265@users.noreply.github.com>
2025-05-11 19:20:49 +0530

    Update webseries.js

--- webseries.js
+++ webseries.js
@@ -20,151 +20,298 @@ const webSeries = [
         ]
     },
   {
-    title: "Khauf (2025) S01",
-    img: "https://image.tmdb.org/t/p/original/puwKd7W9iCocEy11Z1XJ5sN2RaG.jpg",
-    description: "When a young boy vanishes, a small town uncovers a mystery involving secret experiments, terrifying supernatural forces, and one strange little girl.",
-    downloadLinks: [
-      { quality: "1080p AMZN DS4K 7.03GB", link: "https://hubcloud.bz/drive/g65qi3erg41gyr4" },
-      { quality: "1080p AMZN DS4K 7.87GB", link: "https://hubcloud.bz/drive/nbz5u12qdjmclgn" }
+    "title": "Khauf S01 2025 Hindi Completed Web Series HEVC ESub",
+    "download_links": [
+      {
+        "size": "7.03GB",
+        "quality": "1080p-hevc",
+        "url": "https://hubcloud.bz/drive/g65qi3erg41gyr4"
+      },
+      {
+        "size": "7.87GB",
+        "quality": "1080p-hevc",
+        "url": "https://hubcloud.bz/drive/nbz5u12qdjmclgn"
+      }
     ]
   },
   {
-    title: "Moon Knight (2022)",
-    img: "https://image.tmdb.org/t/p/original/5kT3iyseQiVMxUzDVH2gl1MOy4.jpg",
-    description: "When Steven Grant, a mild-mannered gift-shop employee, becomes plagued with blackouts and memories of another life, he discovers he has dissociative identity disorder and shares a body with mercenary Marc Spector. As Steven/Marc’s enemies converge upon them, they must navigate their complex identities while thrust into a deadly mystery among the powerful gods of Egypt.",
-    downloadLinks: [
-      { quality: "1080p Bluray S01 ZIP", link: "https://vifix.site/gdflix/YsVUM3pVym" }
+    "title": "Moon Knight S01 2022 Hindi Completed Web Series HEVC ESub",
+    "download_links": [
+      {
+        "quality": "1080p-hevc",
+        "url": "https://vifix.site/gdflix/YsVUM3pVym"
+      }
     ]
   },
   {
-    title: "Mirzapur (2018) S01",
-    img: "https://image.tmdb.org/t/p/original/p0qM8hhlMF5DuxHBzl2EZR6TehX.jpg",
-    description: "The iron-fisted Akhandanand Tripathi is a millionaire carpet exporter and the mafia don of Mirzapur. His son, Munna, is an unworthy, power-hungry heir who will stop at nothing to inherit his father's legacy. An incident at a wedding procession forces him to cross paths with Ramakant Pandit, an upstanding lawyer, and his sons, Guddu and Bablu. It snowballs into a game of ambition, power and greed that threatens the fabric of this lawless city.",
-    downloadLinks: [
-      { quality: "1080p HEVC AMZN WEB-DL MUL ZIP", link: "https://new6.gdflix.dad/file/2yyluWSS3E" },
-      { quality: "1080p AV1 NF WEB-DL MUL", link: "https://new6.gdflix.dad/file/44pY8DVIF3" }
+    "title": "Mirzapur S01 2018 Hindi Completed Web Series HEVC ESub",
+    "download_links": [
+      {
+        "quality": "1080p-hevc",
+        "url": "https://new6.gdflix.dad/file/2yyluWSS3E"
+      },
+      {
+        "quality": "1080p-av1",
+        "url": "https://new6.gdflix.dad/file/44pY8DVIF3"
+      }
     ]
   },
   {
-    title: "Mirzapur (2018) S02",
-    img: "https://image.tmdb.org/t/p/original/nQyM0gixhRQuwZmv383v02qf8dE.jpg",
-    description: "The iron-fisted Akhandanand Tripathi is a millionaire carpet exporter and the mafia don of Mirzapur. His son, Munna, is an unworthy, power-hungry heir who will stop at nothing to inherit his father's legacy. An incident at a wedding procession forces him to cross paths with Ramakant Pandit, an upstanding lawyer, and his sons, Guddu and Bablu. It snowballs into a game of ambition, power and greed that threatens the fabric of this lawless city.",
-    downloadLinks: [
-      { quality: "1080p HEVC AMZN WEB-DL MUL ZIP", link: "https://new6.gdflix.dad/file/bfTaUUjtm9" },
-      { quality: "1080p AV1 NF WEB-DL MUL", link: "https://new6.gdflix.dad/file/39VZBYIMNz" }
+    "title": "Mirzapur S02 2018 Hindi Completed Web Series HEVC ESub",
+    "download_links": [
+      {
+        "quality": "1080p-hevc",
+        "url": "https://new6.gdflix.dad/file/bfTaUUjtm9"
+      },
+      {
+        "quality": "1080p-av1",
+        "url": "https://new6.gdflix.dad/file/39VZBYIMNz"
+      }
     ]
   },
   {
-    title: "Mirzapur (2018) S03",
-    img: "https://image.tmdb.org/t/p/original/tzOnlogwpQPPC6AUhYSz8URuRFD.jpg",
-    description: "The iron-fisted Akhandanand Tripathi is a millionaire carpet exporter and the mafia don of Mirzapur. His son, Munna, is an unworthy, power-hungry heir who will stop at nothing to inherit his father's legacy. An incident at a wedding procession forces him to cross paths with Ramakant Pandit, an upstanding lawyer, and his sons, Guddu and Bablu. It snowballs into a game of ambition, power and greed that threatens the fabric of this lawless city.",
-    downloadLinks: [
-      { quality: "1080p HEVC AMZN WEB-DL MUL ZIP", link: "https://new6.gdflix.dad/file/acVAQBWquN" },
-      { quality: "1080p AV1 NF WEB-DL MUL", link: "https://new6.gdflix.dad/file/Q3vvnrOtMO" },
-      { quality: "2180p BONUS EP NF WEB-DL HIN", link: "https://hubcloud.bz/drive/tacxe6aff6tuqqx" }
+    "title": "Mirzapur S03 2018 Hindi Completed Web Series HEVC ESub",
+    "download_links": [
+      {
+        "quality": "1080p-hevc",
+        "url": "https://new6.gdflix.dad/file/acVAQBWquN"
+      },
+      {
+        "quality": "1080p-av1",
+        "url": "https://new6.gdflix.dad/file/Q3vvnrOtMO"
+      },
+      {
+        "quality": "2180p-hevc",
+        "url": "https://hubcloud.bz/drive/tacxe6aff6tuqqx"
+      }
     ]
   },
   {
-    title: "Stranger Things S01",
-    img: "https://image.tmdb.org/t/p/original/jS3pbMv7CCajFRd52eK77R0scku.jpg",
-    description: "When a young boy vanishes, a small town uncovers a mystery involving secret experiments, terrifying supernatural forces, and one strange little girl.",
-    downloadLinks: [
-      { quality: "1080p HEVC NF WEB-DL MUL", link: "https://new6.gdflix.dad/file/ZIHlxGlRvG" },
-      { quality: "1080p AV1 NF WEB-DL MUL", link: "https://new6.gdflix.dad/file/07w8XtoMLN" }
+    "title": "Stranger Things S01 Hindi Completed Web Series HEVC ESub",
+    "download_links": [
+      {
+        "quality": "1080p-hevc",
+        "url": "https://new6.gdflix.dad/file/ZIHlxGlRvG"
+      },
+      {
+        "quality": "1080p-av1",
+        "url": "https://new6.gdflix.dad/file/07w8XtoMLN"
+      }
     ]
   },
   {
-    title: "Stranger Things S02",
-    img: "https://image.tmdb.org/t/p/original/uAc35R8pBa7ykvDfgy1tQs76ETP.jpg",
-    description: "Madhu moves into a hostel in Delhi to start afresh, but her room hides a sinister presence that begins to get stronger each day, alarming the other women on her floor. They know what the room hides and desperately want her gone before it is too late for them all.",
-    downloadLinks: [
-      { quality: "1080p AMZN WEB-DL HIN ZIP", link: "https://gdflix.dad/xfile/r97hySr6JG" }
+    "title": "Stranger Things S02 Hindi Completed Web Series HEVC ESub",
+    "download_links": [
+      {
+        "quality": "1080p-hevc",
+        "url": "https://gdflix.dad/xfile/r97hySr6JG"
+      }
     ]
   },
   {
-    title: "Stranger Things S03",
-    img: "https://image.tmdb.org/t/p/original/3Emf1Nal5M3h3gsw8KGcs21IhNG.jpg",
-    description: "When a young boy vanishes, a small town uncovers a mystery involving secret experiments, terrifying supernatural forces, and one strange little girl.",
-    downloadLinks: [
-      { quality: "1080p HEVC NF WEB-DL MUL", link: "https://new6.gdflix.dad/file/a5GnxcYQwa" },
-      { quality: "1080p AV1 NF WEB-DL MUL", link: "https://new6.gdflix.dad/file/esdn2Otz5S" }
+    "title": "Stranger Things S03 Hindi Completed Web Series HEVC ESub",
+    "download_links": [
+      {
+        "quality": "1080p-hevc",
+        "url": "https://new6.gdflix.dad/file/a5GnxcYQwa"
+      },
+      {
+        "quality": "1080p-av1",
+        "url": "https://new6.gdflix.dad/file/esdn2Otz5S"
+      }
     ]
   },
   {
-    title: "Stranger Things S04",
-    img: "https://image.tmdb.org/t/p/original/dOzW7UEVlQJs1WtNHfIe9JzcKKv.jpg",
-    description: "When a young boy vanishes, a small town uncovers a mystery involving secret experiments, terrifying supernatural forces, and one strange little girl.",
-    downloadLinks: [
-      { quality: "1080p HEVC NF WEB-DL MUL", link: "https://new6.gdflix.dad/file/HQl8QNbspx" },
-      { quality: "1080p AV1 NF WEB-DL MUL", link: "https://new6.gdflix.dad/file/shESG15vae" }
+    "title": "Stranger Things S04 Hindi Completed Web Series HEVC ESub",
+    "download_links": [
+      {
+        "quality": "1080p-hevc",
+        "url": "https://new6.gdflix.dad/file/HQl8QNbspx"
+      },
+      {
+        "quality": "1080p-av1",
+        "url": "https://new6.gdflix.dad/file/shESG15vae"
+      }
     ]
   },
   {
-    title: "Game of Thrones S01 (2011)",
-    img: "https://image.tmdb.org/t/p/original/fw9hNiJO0U5MUg2XGyCOlTS3jdS.jpg",
-    description: "Seven noble families fight for control of the mythical land of Westeros. Friction between the houses leads to full-scale war. All while a very ancient evil awakens in the farthest north...",
-    downloadLinks: [
-      { quality: "4K UHD GOT S01E01 MUL", link: "https://new6.gdflix.dad/file/NseSwvJISK" },
-      { quality: "4K UHD GOT S01E02 MUL", link: "https://new6.gdflix.dad/file/RgNpAjB7q2" },
-      { quality: "4K UHD GOT S01E03 MUL", link: "https://new6.gdflix.dad/file/UX512ZJ1RX" },
-      { quality: "4K UHD GOT S01E04 MUL", link: "https://new6.gdflix.dad/file/pEHWCweR5h" },
-      { quality: "4K UHD GOT S01E05 MUL", link: "https://new6.gdflix.dad/file/Z7mDIPdaf2" },
-      { quality: "4K UHD GOT S01E06 MUL", link: "https://new6.gdflix.dad/file/u3b7i9NchN" },
-      { quality: "4K UHD GOT S01E07 MUL", link: "https://new6.gdflix.dad/file/3hAROJM077" },
-      { quality: "4K UHD GOT S01E08 MUL", link: "https://new6.gdflix.dad/file/11Rmt2jijl" },
-      { quality: "4K UHD GOT S01E09 MUL", link: "https://new6.gdflix.dad/file/4A5f89bW5i" },
-      { quality: "4K UHD GOT S01E10 MUL", link: "https://new6.gdflix.dad/file/7vNcjzjmCc" }
+    "title": "Game of Thrones S01 2011 Hindi Completed Web Series HEVC ESub",
+    "download_links": [
+      {
+        "quality": "4k-hevc",
+        "url": "https://new6.gdflix.dad/file/NseSwvJISK"
+      },
+      {
+        "quality": "4k-hevc",
+        "url": "https://new6.gdflix.dad/file/RgNpAjB7q2"
+      },
+      {
+        "quality": "4k-hevc",
+        "url": "https://new6.gdflix.dad/file/UX512ZJ1RX"
+      },
+      {
+        "quality": "4k-hevc",
+        "url": "https://new6.gdflix.dad/file/pEHWCweR5h"
+      },
+      {
+        "quality": "4k-hevc",
+        "url": "https://new6.gdflix.dad/file/Z7mDIPdaf2"
+      },
+      {
+        "quality": "4k-hevc",
+        "url": "https://new6.gdflix.dad/file/u3b7i9NchN"
+      },
+      {
+        "quality": "4k-hevc",
+        "url": "https://new6.gdflix.dad/file/3hAROJM077"
+      },
+      {
+        "quality": "4k-hevc",
+        "url": "https://new6.gdflix.dad/file/11Rmt2jijl"
+      },
+      {
+        "quality": "4k-hevc",
+        "url": "https://new6.gdflix.dad/file/4A5f89bW5i"
+      },
+      {
+        "quality": "4k-hevc",
+        "url": "https://new6.gdflix.dad/file/7vNcjzjmCc"
+      }
     ]
   },
   {
-    title: "Game of Thrones S02 (2011)",
-    img: "https://image.tmdb.org/t/p/original/bw6DlqljVFIinhBA7uDSNha6Lnp.jpg",
-    description: "The war for the Iron Throne intensifies as five kings claim their right to rule the land. The Night's Watch discovers something deadly beyond the Wall...",
-    downloadLinks: [
-      { quality: "4K UHD GOT S02E01 MUL", link: "https://new6.gdflix.dad/file/cHV93uVc8E" },
-      { quality: "4K UHD GOT S02E02 MUL", link: "https://new6.gdflix.dad/file/FPfbQeCaZF" },
-      { quality: "4K UHD GOT S02E03 MUL", link: "https://new6.gdflix.dad/file/LvmFY2ps7p" },
-      { quality: "4K UHD GOT S02E04 MUL", link: "https://new6.gdflix.dad/file/hKaPPPWmkU" },
-      { quality: "4K UHD GOT S02E05 MUL", link: "https://new6.gdflix.dad/file/VagvJmFum9" },
-      { quality: "4K UHD GOT S02E06 MUL", link: "https://new6.gdflix.dad/file/tml4DfDSBu" },
-      { quality: "4K UHD GOT S02E07 MUL", link: "https://new6.gdflix.dad/file/rmmxf09mUX" },
-      { quality: "4K UHD GOT S02E08 MUL", link: "https://new6.gdflix.dad/file/2qxdf9ugMa" },
-      { quality: "4K UHD GOT S02E09 MUL", link: "https://new6.gdflix.dad/file/QuWYUkSUBh" },
-      { quality: "4K UHD GOT S02E10 MUL", link: "https://new6.gdflix.dad/file/QuWYUkSUBh" }
+    "title": "Game of Thrones S02 2011 Hindi Completed Web Series HEVC ESub",
+    "download_links": [
+      {
+        "quality": "4k-hevc",
+        "url": "https://new6.gdflix.dad/file/cHV93uVc8E"
+      },
+      {
+        "quality": "4k-hevc",
+        "url": "https://new6.gdflix.dad/file/FPfbQeCaZF"
+      },
+      {
+        "quality": "4k-hevc",
+        "url": "https://new6.gdflix.dad/file/LvmFY2ps7p"
+      },
+      {
+        "quality": "4k-hevc",
+        "url": "https://new6.gdflix.dad/file/hKaPPPWmkU"
+      },
+      {
+        "quality": "4k-hevc",
+        "url": "https://new6.gdflix.dad/file/VagvJmFum9"
+      },
+      {
+        "quality": "4k-hevc",
+        "url": "https://new6.gdflix.dad/file/tml4DfDSBu"
+      },
+      {
+        "quality": "4k-hevc",
+        "url": "https://new6.gdflix.dad/file/rmmxf09mUX"
+      },
+      {
+        "quality": "4k-hevc",
+        "url": "https://new6.gdflix.dad/file/2qxdf9ugMa"
+      },
+      {
+        "quality": "4k-hevc",
+        "url": "https://new6.gdflix.dad/file/QuWYUkSUBh"
+      },
+      {
+        "quality": "4k-hevc",
+        "url": "https://new6.gdflix.dad/file/QuWYUkSUBh"
+      }
     ]
   },
   {
-    title: "Game of Thrones S03 (2011)",
-    img: "https://image.tmdb.org/t/p/original/u3bZgnGQ9T01sWNhyveQz0wH0Hl.jpg",
-    description: "The war for the Iron Throne intensifies as five kings claim their right to rule the land. The Night's Watch discovers something deadly beyond the Wall...",
-    downloadLinks: [
-      { quality: "4K UHD GOT S03E01 MUL", link: "https://new6.gdflix.dad/file/cXcU7PCJKG" },
-      { quality: "4K UHD GOT S03E02 MUL", link: "https://new6.gdflix.dad/file/FLpQAhmw1B" },
-      { quality: "4K UHD GOT S03E03 MUL", link: "https://new6.gdflix.dad/file/LVqMn15Hku" },
-      { quality: "4K UHD GOT S03E04 MUL", link: "https://new6.gdflix.dad/file/qvMGiBPAhp" },
-      { quality: "4K UHD GOT S03E05 MUL", link: "https://new6.gdflix.dad/file/zXjvkBTm4h" },
-      { quality: "4K UHD GOT S03E06 MUL", link: "https://new6.gdflix.dad/file/2F945aDfyl" },
-      { quality: "4K UHD GOT S03E07 MUL", link: "https://new6.gdflix.dad/file/CtBz8RovTK" },
-      { quality: "4K UHD GOT S03E08 MUL", link: "https://new6.gdflix.dad/file/uKFSqvieLp" },
-      { quality: "4K UHD GOT S03E09 MUL", link: "https://new6.gdflix.dad/file/1chuZNjzDd" },
-      { quality: "4K UHD GOT S03E10 MUL", link: "https://new6.gdflix.dad/file/kBFQZmJd6x" }
+    "title": "Game of Thrones S03 2011 Hindi Completed Web Series HEVC ESub",
+    "download_links": [
+      {
+        "quality": "4k-hevc",
+        "url": "https://new6.gdflix.dad/file/cXcU7PCJKG"
+      },
+      {
+        "quality": "4k-hevc",
+        "url": "https://new6.gdflix.dad/file/FLpQAhmw1B"
+      },
+      {
+        "quality": "4k-hevc",
+        "url": "https://new6.gdflix.dad/file/LVqMn15Hku"
+      },
+      {
+        "quality": "4k-hevc",
+        "url": "https://new6.gdflix.dad/file/qvMGiBPAhp"
+      },
+      {
+        "quality": "4k-hevc",
+        "url": "https://new6.gdflix.dad/file/zXjvkBTm4h"
+      },
+      {
+        "quality": "4k-hevc",
+        "url": "https://new6.gdflix.dad/file/2F945aDfyl"
+      },
+      {
+        "quality": "4k-hevc",
+        "url": "https://new6.gdflix.dad/file/CtBz8RovTK"
+      },
+      {
+        "quality": "4k-hevc",
+        "url": "https://new6.gdflix.dad/file/uKFSqvieLp"
+      },
+      {
+        "quality": "4k-hevc",
+        "url": "https://new6.gdflix.dad/file/1chuZNjzDd"
+      },
+      {
+        "quality": "4k-hevc",
+        "url": "https://new6.gdflix.dad/file/kBFQZmJd6x"
+      }
     ]
   },
   {
-    title: "Game of Thrones S04 (2011)",
-    img: "https://image.tmdb.org/t/p/original/gdeLQ5odsYE5EZ6hhnm2PwnjycB.jpg",
-    description: "Seven noble families fight for control of the mythical land of Westeros. Friction between the houses leads to full-scale war. All while a very ancient evil awakens in the farthest north...",
-    downloadLinks: [
-      { quality: "4K UHD GOT S04E01 MUL", link: "https://new6.gdflix.dad/file/qQnuXcjcu5" },
-      { quality: "4K UHD GOT S04E02 MUL", link: "https://new6.gdflix.dad/file/z0Xu8H9SV1" },
-      { quality: "4K UHD GOT S04E03 MUL", link: "https://new6.gdflix.dad/file/b7bGr4q64g" },
-      { quality: "4K UHD GOT S04E04 MUL", link: "https://new6.gdflix.dad/file/cJJ27a8ZCP" },
-      { quality: "4K UHD GOT S04E05 MUL", link: "https://new6.gdflix.dad/file/jeCxHDfZKe" },
-      { quality: "4K UHD GOT S04E06 MUL", link: "https://new6.gdflix.dad/file/SxCZjWPhdP" },
-      { quality: "4K UHD GOT S04E07 MUL", link: "https://new6.gdflix.dad/file/H9AIwR8GmB" },
-      { quality: "4K UHD GOT S04E08 MUL", link: "https://new6.gdflix.dad/file/e3NBZi0uBR" },
-      { quality: "4K UHD GOT S04E09 MUL", link: "https://new6.gdflix.dad/file/EKYzhh9BIr" },
-      { quality: "4K UHD GOT S04E10 MUL", link: "https://new6.gdflix.dad/file/fo2mRHrlfp" }
+    "title": "Game of Thrones S04 2011 Hindi Completed Web Series HEVC ESub",
+    "download_links": [
+      {
+        "quality": "4k-hevc",
+        "url": "https://new6.gdflix.dad/file/qQnuXcjcu5"
+      },
+      {
+        "quality": "4k-hevc",
+        "url": "https://new6.gdflix.dad/file/z0Xu8H9SV1"
+      },
+      {
+        "quality": "4k-hevc",
+        "url": "https://new6.gdflix.dad/file/b7bGr4q64g"
+      },
+      {
+        "quality": "4k-hevc",
+        "url": "https://new6.gdflix.dad/file/cJJ27a8ZCP"
+      },
+      {
+        "quality": "4k-hevc",
+        "url": "https://new6.gdflix.dad/file/jeCxHDfZKe"
+      },
+      {
+        "quality": "4k-hevc",
+        "url": "https://new6.gdflix.dad/file/SxCZjWPhdP"
+      },
+      {
+        "quality": "4k-hevc",
+        "url": "https://new6.gdflix.dad/file/H9AIwR8GmB"
+      },
+      {
+        "quality": "4k-hevc",
+        "url": "https://new6.gdflix.dad/file/e3NBZi0uBR"
+      },
+      {
+        "quality": "4k-hevc",
+        "url": "https://new6.gdflix.dad/file/EKYzhh9BIr"
+      },
+      {
+        "quality": "4k-hevc",
+        "url": "https://new6.gdflix.dad/file/fo2mRHrlfp"
+      }
     ]
   },
     {
