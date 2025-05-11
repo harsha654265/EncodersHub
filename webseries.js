66ba332b59c00f6c61b99164403a140b154f001c
Harsha Vardhan <129319351+harsha654265@users.noreply.github.com>
2025-05-11 10:45:56 +0530

    Update webseries.js

--- webseries.js
+++ webseries.js
@@ -1,31 +1,29 @@
-const webSeries = [
-  {
-    title: "Stranger Things S01",
-    img: "https://image.tmdb.org/t/p/original/jS3pbMv7CCajFRd52eK77R0scku.jpg",
-    genres: ["Science Fiction", "Horror", "Mystery"],
-    runtime: "50m per episode",
-    releaseDate: "2016-07-15",
-    tags: ["Supernatural", "Mystery", "80s", "English", "New"],
-    heroes: ["Finn Wolfhard", "Gaten Matarazzo"],
-    heroines: ["Millie Bobby Brown", "Winona Ryder"],
-    directors: ["The Duffer Brothers"],
-    downloadLinks: [
-      { quality: "1080p HEVC NF WEB-DL MUL", link: "https://new6.gdflix.dad/file/ZIHlxGlRvG" },
-      { quality: "1080p AV1 NF WEB-DL MUL", link: "https://new6.gdflix.dad/file/07w8XtoMLN" }
-    ]
-  },
-  {
-    title: "Stranger Things S02",
-    img: "https://image.tmdb.org/t/p/original/uAc35R8pBa7ykvDfgy1tQs76ETP.jpg",
-    genres: ["Science Fiction", "Horror", "Mystery"],
-    runtime: "50m per episode",
-    releaseDate: "2017-10-27",
-    tags: ["Supernatural", "Mystery", "80s", "English", "New"],
-    heroes: ["Finn Wolfhard", "Gaten Matarazzo"],
-    heroines: ["Millie Bobby Brown", "Winona Ryder"],
-    directors: ["The Duffer Brothers"],
-    downloadLinks: [
-      { quality: "1080p AMZN WEB-DL HIN ZIP", link: "https://new6.gdflix.dad/file/pxhmpa4wkTAQYPI" }
-    ]
-  }
-];
+web_series = [
+    {
+        "title": "The Royals S01 2025",
+        "release_date": "2025",
+        "download_links": [
+            {"size": "1gb", "quality": "480p-hevc", "url": "https://dlfiles.filesdl.in/dl/fdownload.php?id=aHR0cHM6Ly9waG90b3MuZ29vZ2xlLmNvbS9zaGFyZS9BRjFRaXBPQTFFM2tNelc1a1ViYjI4UGc1MzZWR3FaUEhaTEJjdkpRUzBkcTBkS1RVSkNadlZnNTZjWWF0Y2ZQT3QtRjJ3P2tleT1WVzlYY0VkSlJuUm5jVUoxY1UxTVFWSmFlRXRIVjNod2MzbFhRazVC&token=MTAwNTI1"},
+            {"size": "2gb", "quality": "720p-hevc", "url": "https://dlfiles.filesdl.in/dl/fdownload.php?id=aHR0cHM6Ly9waG90b3MuZ29vZ2xlLmNvbS9zaGFyZS9BRjFRaXBPSFAtbWZSVTh0UUJWbUtLR2VDSUJIVHVPcHdSb0hRRUFaSlg2RG95WG54UEVQQ0lhRWdCLTBGM0dId01lYXNBP2tleT1ZakZ1YjJkeFVGRk1NV0Y1V25ZeVdrdHdlRFUyUlVWTlVXbEVZWFJS&token=MTAwNTI1"},
+            {"size": "5gb", "quality": "1080p-hevc", "url": "https://dlfiles.filesdl.in/dl/fdownload.php?id=aHR0cHM6Ly9waG90b3MuZ29vZ2xlLmNvbS9zaGFyZS9BRjFRaXBQVU9Yb01RZWNYemVNYWk3dE5FN3RjcDdscjJFUVZnT0NTb0ZCTEJ2NlJJY1A0MTZNTkFXZG9NT0VFVWlGcEt3P2tleT1SREV0ZW5ocmJtZHJPRGhJVG1GNlRWZEpVMWxETjFWU1JWSnVTbkJu&token=MTAwNTI1"}
+        ]
+    },
+    {
+        "title": "Gram Chikitsalay S01 2025",
+        "release_date": "2025",
+        "download_links": [
+            {"size": "650mb", "quality": "480p-hevc", "url": "https://dlfiles.filesdl.in/dl/fdownload.php?id=aHR0cHM6Ly9waG90b3MuZ29vZ2xlLmNvbS9zaGFyZS9BRjFRaXBNYk9venlVMHpZbDRNZWFLRnpaV3BPSWZ6RWdoSzcySlVGSnp1WWRzOWlKVmFUWUpyYURqVGRxUnFaMzFlRUdnP2tleT1NRXRmTlV0aWJqTmFibUZJTldGdGRGWjZURXgyYVhVd1JqSkJjbWhu&token=MTAwNTI1"},
+            {"size": "1.1gb", "quality": "720p-hevc", "url": "https://dlfiles.filesdl.in/dl/fdownload.php?id=aHR0cHM6Ly9waG90b3MuZ29vZ2xlLmNvbS9zaGFyZS9BRjFRaXBPSVJ4UVpEdzFKU2U4dEthU0k5NXhQcGdpVDVnVVZaNUlPTEVyckptWVpLV2t3ZkNSeml4WXB3Ylp4RW05NEZnP2tleT1aVGxzVVZGek1WOW9jMTlGZDJORVFWaHJRbkJKUkV4VE1GUkdjV0pC&token=MTAwNTI1"},
+            {"size": "2.3gb", "quality": "1080p-hevc", "url": "https://dlfiles.filesdl.in/dl/fdownload.php?id=aHR0cHM6Ly9waG90b3MuZ29vZ2xlLmNvbS9zaGFyZS9BRjFRaXBNMTA0S1JnMmh5OVI2NC1aWTdCTGtpWWcxeEFBLUdzQXRYdWZyVWpHMFZ3c293UlhRaGhFaU9tVTJ0MFc2eEZ3P2tleT1WVFl3TkRCbVVHMUNNRUZTWjIxeE0zVmpObVkyU1ZoV2NEQm9MV0pu&token=MTAwNTI1"}
+        ]
+    },
+    {
+        "title": "Peaky Blinders S02 2014",
+        "release_date": "2014",
+        "download_links": [
+            {"size": "1gb", "quality": "480p-hevc", "url": "https://dlfiles.filesdl.in/dl/fdownload.php?id=aHR0cHM6Ly9waG90b3MuZ29vZ2xlLmNvbS9zaGFyZS9BRjFRaXBPQnFUUWh2cEZma29wM01DMHF3WURELVYzT0t3bGx1cDU3NEhBYzRNVzNsM21WaDFuVXBqOFNiSUVjaW9WVXdBP2tleT1aV2xKTjBkUk4waGFUM2RxZFZCeVZVYzRXRzR5ZGpNMlkyaGhjWFIz&token=MTAwNTI1"},
+            {"size": "2gb", "quality": "720p-hevc", "url": "https://dlfiles.filesdl.in/dl/fdownload.php?id=aHR0cHM6Ly9waG90b3MuZ29vZ2xlLmNvbS9zaGFyZS9BRjFRaXBNeF9Ob0ZEaWJYSnh5WDVMdW5HY3B5NHhoZFdfZmtoM2FqT01sQ1FvTGhpdGxDd013d1dsSktLcWY5eC1ycnVRP2tleT1OekpzUm5saFVFbEhWVWRFUlRONWJFODVMVk42YkZCeVNVTXlPWGxC&token=MTAwNTI1"},
+            {"size": "4.5gb", "quality": "1080p-hevc", "url": "https://dlfiles.filesdl.in/dl/fdownload.php?id=aHR0cHM6Ly9waG90b3MuZ29vZ2xlLmNvbS9zaGFyZS9BRjFRaXBNdkYxWVh3ZTIzbEJwbjM4UkdOY04yaEI4MXhKa1FicXNxNnBZbVdzLXpJWS1Id2lLRTBKUXA2RUFJVXVHZFpRP2tleT1ZakJOUms5Q1RWZHZObXMxZUc4dGVrZFdTbUZMYlZSMVpGUlBaSFZu&token=MTAwNTI1"}
+        ]
+    }
+]
