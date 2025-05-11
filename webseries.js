25cb94444b01d5efadca217c2f6e1327189f08bb
Harsha Vardhan <129319351+harsha654265@users.noreply.github.com>
2025-05-11 10:20:39 +0530

    Create webseries.js

---
+++ webseries.js
@@ -0,0 +1,31 @@
+const webSeries = [
+  {
+    title: "Stranger Things S01",
+    img: "https://image.tmdb.org/t/p/original/jS3pbMv7CCajFRd52eK77R0scku.jpg",
+    genres: ["Science Fiction", "Horror", "Mystery"],
+    runtime: "50m per episode",
+    releaseDate: "2016-07-15",
+    tags: ["Supernatural", "Mystery", "80s", "English", "New"],
+    heroes: ["Finn Wolfhard", "Gaten Matarazzo"],
+    heroines: ["Millie Bobby Brown", "Winona Ryder"],
+    directors: ["The Duffer Brothers"],
+    downloadLinks: [
+      { quality: "1080p HEVC NF WEB-DL MUL", link: "https://new6.gdflix.dad/file/ZIHlxGlRvG" },
+      { quality: "1080p AV1 NF WEB-DL MUL", link: "https://new6.gdflix.dad/file/07w8XtoMLN" }
+    ]
+  },
+  {
+    title: "Stranger Things S02",
+    img: "https://image.tmdb.org/t/p/original/uAc35R8pBa7ykvDfgy1tQs76ETP.jpg",
+    genres: ["Science Fiction", "Horror", "Mystery"],
+    runtime: "50m per episode",
+    releaseDate: "2017-10-27",
+    tags: ["Supernatural", "Mystery", "80s", "English", "New"],
+    heroes: ["Finn Wolfhard", "Gaten Matarazzo"],
+    heroines: ["Millie Bobby Brown", "Winona Ryder"],
+    directors: ["The Duffer Brothers"],
+    downloadLinks: [
+      { quality: "1080p AMZN WEB-DL HIN ZIP", link: "https://new6.gdflix.dad/file/pxhmpa4wkTAQYPI" }
+    ]
+  }
+];
